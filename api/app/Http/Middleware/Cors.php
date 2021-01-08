7fafe3088d7770ff8d4dd5c290633b257a0e1b7b
Omar Guevara <omardevelopersoft@gmail.com>
2021-01-08 14:26:57 -0500

    cors

--- api/app/Http/Middleware/Cors.php
+++ api/app/Http/Middleware/Cors.php
@@ -18,21 +18,23 @@ class Cors
     {
         $handle = $next($request);
 
-        // if ($handle->isMethod('OPTIONS')){
-
-        //     $handle = Response::make();
-        // } 
-
         if(method_exists($handle, 'header'))
         {   
             $handle->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, Application, Origin')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, Application, Origin, Access-Control-Request-Method')
             ->header('Content-Type', '*')
             ->header('Accept', 'application/json');
         } else {
 
             $handle->headers->set('Access-Control-Allow-Origin' , '*');
+        }
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if($method == "OPTIONS") {
+
+            die();
         }
 
         return $handle;
