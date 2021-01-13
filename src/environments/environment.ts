2d91241ccb66380f4f96381320a6d8c1e1b1aa77
yunerkis <yumycary@gmail.com>
2021-01-13 11:04:32 -0500

    Fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  url: 'https://api.saludantioquia.com',
-  // url: 'http://localhost:8000',
+  //url: 'https://api.saludantioquia.com',
+  url: 'http://localhost:8000',
 };
 
 /*
